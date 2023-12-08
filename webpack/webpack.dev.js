a834826d005993465b969d6c36ce398b0ad5f5ca
Pridesd <whwotjr98@naver.com>
2023-12-08 22:49:03 +0900

    chore(webpackbar): set webpackbar

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,16 @@
 const { merge } = require('webpack-merge');
+const WebpackBar = require('webpackbar');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval',
+  plugins: [
+    new WebpackBar({
+      name: 'Loading',
+      color: 'blue',
+    }),
+  ],
   devServer: {
     historyApiFallback: true,
     port: 3000,
