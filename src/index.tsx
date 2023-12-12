56ec03801502041993326429b24cc38891a64c96
Pridesd <whwotjr98@naver.com>
2023-12-12 23:17:37 +0900

    chore(index): set dev mock setting

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,22 @@ if (!rootNode) {
   throw new Error('Failed to find the root element');
 }
 
-ReactDOM.createRoot(rootNode).render(
-  <React.StrictMode>
-    <Route />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(rootNode).render(
+    <React.StrictMode>
+      <Route />
+    </React.StrictMode>,
+  );
+});
