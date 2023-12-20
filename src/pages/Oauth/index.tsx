f2766a1613c80482072b8b5070e8603965353419
Pridesd <whwotjr98@naver.com>
2023-12-20 23:00:46 +0900

    style(loadingLottie): set loading lottie

--- src/pages/Oauth/index.tsx
+++ src/pages/Oauth/index.tsx
@@ -1,5 +1,19 @@
+import { Stack } from '@mui/material';
+import Lottie from 'lottie-react';
+
+import runningLottie from '@/assets/running_lottie.json';
+
 const Oauth: React.FC = () => {
-  return <>소셜 로그인 중</>;
+  return (
+    <Stack alignItems="center" justifyContent="center" minHeight="100vh">
+      <Lottie
+        alt="로딩 이미지"
+        height="5rem"
+        width="5rem"
+        animationData={runningLottie}
+      />
+    </Stack>
+  );
 };
 
 export default Oauth;
