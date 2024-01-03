1b0ab1232dded3e7c924143a69a8326369833621
Pridesd <whwotjr98@naver.com>
2024-01-03 19:54:35 +0900

    feat(Signup): add completed path

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
+import SignupComplete from './SignupComplete';
 import SignupGuide from './SignupGuide';
 import SignupIntro from './SignupIntro';
 import SignupVi from './SignupVi';
+
+import { BROWSER_PATH } from '@/constants/path';
 
 const Signup: React.FC = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const type = searchParams.get('type');
+  const isCompleted = Boolean(searchParams.get('isCompleted'));
   /**
    *
    */
   const renderContent = () => {
     if (!type) {
       return <SignupIntro />;
+    }
+
+    if (isCompleted) {
+      return <SignupComplete />;
     }
 
     switch (type) {
@@ -31,7 +39,7 @@ const Signup: React.FC = () => {
   //
   React.useEffect(() => {
     if (type && type !== 'vi' && type !== 'guide') {
-      navigate('/');
+      navigate(BROWSER_PATH.SIGNUP);
     }
   }, [type]);
 
