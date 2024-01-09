04871e6b4e020dbc5deb6718c778d7e38bf4f253
Pridesd <whwotjr98@naver.com>
2024-01-09 17:58:58 +0900

    style(color): set group color

---
+++ src/constants/color.ts
@@ -0,0 +1,13 @@
+export const TEAM_COLOR = {
+  A: '#CC5A37',
+  B: '#DB8403',
+  C: '#99940B',
+  D: '#31A515',
+  E: '#2D97A6',
+  P: '#8E8E8E',
+} as const;
+
+export const TYPE_COLOR = {
+  VI: '#900782',
+  GUIDE: '#0C3B82',
+} as const;
