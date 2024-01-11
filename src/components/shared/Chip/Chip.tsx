b97f3d42454ceaab65e5e7495cf80ae39c90a69e
Pridesd <whwotjr98@naver.com>
2024-01-11 14:44:03 +0900

    style(Chip): set Chips

--- src/components/shared/Chip/Chip.tsx
+++ src/components/shared/Chip/Chip.tsx
@@ -14,15 +14,12 @@ const StyledChip = styled.div<{ bgColor?: string; color?: string }>`
   position: relative;
   display: inline-flex;
   align-items: center;
-  -webkit-box-pack: center;
   justify-content: center;
-  flex-grow: 0;
-  flex-shrink: 0;
-  height: 1.25rem;
   padding: 0.25rem 0.5rem;
   border-radius: 0.25rem;
   font-family: 'pretendard', sans-serif;
   font-size: 0.625rem; //10px
+  line-height: 0.625rem;
   font-weight: 700;
   background-color: ${({ bgColor }) => bgColor ?? 'grey'};
   color: ${({ color }) => color ?? '#000'};
