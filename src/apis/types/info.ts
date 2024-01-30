baaf284fb72ad31fd165b3f54123ed7a094d8919
Pridesd <whwotjr98@naver.com>
2024-01-31 00:36:43 +0900

    chore(info): set api type

---
+++ src/apis/types/info.ts
@@ -0,0 +1,255 @@
+import {
+  DisabilityEnum,
+  GenderEnum,
+  RoleEnum,
+  RunningGroup,
+} from '@/types/group';
+
+export type myPageGetResponse = {
+  role: RoleEnum;
+  type: DisabilityEnum;
+  gender: GenderEnum;
+  recordDegree: RunningGroup;
+  name: string;
+
+  totalEvent: number;
+  trainingCnt: number;
+  contestCnt: number;
+};
+
+export type userInfoGetResponse = {
+  userId: string;
+  name: string;
+  type: DisabilityEnum;
+  role: RoleEnum;
+  gender: GenderEnum;
+  phoneNumber: string;
+  recordDegree: RunningGroup;
+  age: number;
+  snsId: string;
+};
+
+export type personalInfoGetRequest = {
+  userId: string;
+};
+
+export type personalInfoGetResponse = {
+  name: string;
+  type: DisabilityEnum;
+  role: RoleEnum;
+  gender: GenderEnum;
+  phoneNumber: string;
+  age: number;
+  snsId: string;
+};
+
+export type userInfoPatchRequest = {
+  name: string;
+  gender: GenderEnum;
+  phoneNumber: string;
+  age: string;
+  snsId: string;
+};
+
+export type userInfoPatchResponse = {
+  name: string;
+  type: DisabilityEnum;
+  role: RoleEnum;
+  gender: GenderEnum;
+  phoneNumber: string;
+  age: number;
+  snsId: string;
+};
+
+export type runningSpecGuideGetRequest = {
+  userId: string;
+};
+
+export type runningSpecGuideGetResponse = {
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  isGuideExp: boolean;
+  runningPlace: string;
+  viName: string | null;
+  viRecord: string | null;
+  viCount: string | null;
+  howToKnow: string[] | null;
+  motive: string | null;
+  guidingPace: RunningGroup;
+  hopePrefs: string | null;
+};
+
+export type runningSpecGuidePatchRequest = {
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  isGuideExp: boolean;
+  runningPlace: string;
+  viName: string | null;
+  viRecord: string | null;
+  viCount: string | null;
+  howToKnow: string[] | null;
+  motive: string | null;
+  guidingPace: RunningGroup;
+  hopePrefs: string | null;
+};
+
+export type runningSpecGuidePatchResponse = {
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  isGuideExp: boolean;
+  runningPlace: string;
+  viName: string | null;
+  viRecord: string | null;
+  viCount: string | null;
+
+  howToKnow: string[] | null;
+  motive: string | null;
+
+  guidingPace: RunningGroup;
+  hopePrefs: string | null;
+};
+
+export type runningSpecViGetRequest = {
+  userId: string;
+};
+
+export type runningSpecViGetResponse = {
+  isRunningExp: boolean;
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  runningPlace: string | null;
+  guideName: string | null;
+
+  howToKnow: string[] | null;
+  motive: string | null;
+
+  hopePrefs: string | null;
+};
+
+export type runningSpecViPatchRequest = {
+  isRunningExp: boolean;
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  runningPlace: string | null;
+  guideName: string | null;
+
+  howToKnow: string[] | null;
+  motive: string | null;
+
+  hopePrefs: string | null;
+};
+
+export type runningSpecViPatchResponse = {
+  isRunningExp: boolean;
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  runningPlace: string | null;
+  guideName: string | null;
+
+  howToKnow: string[] | null;
+  motive: string | null;
+
+  hopePrefs: string | null;
+};
+
+export type permissionGetRequest = {
+  userId: string;
+};
+
+export type permissionGetResponse = {
+  privacy: boolean;
+  portraitRights: boolean;
+};
+
+export type permissionPatchRequest = {
+  privacy: boolean;
+  portraitRights: boolean;
+};
+
+export type permissionPatchResponse = {
+  privacy: boolean;
+  portraitRights: boolean;
+};
+
+export type profileGetRequest = {
+  userId: string;
+};
+
+export type profileGetResponse = {
+  userId: string;
+  role: RoleEnum;
+  type: DisabilityEnum;
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  phoneNumber: string;
+  isOpenNumber: boolean;
+  age: number;
+  snsId: string;
+  isOpenSns: boolean;
+
+  totalEvent: number;
+  trainingCnt: number;
+  contestCnt: number;
+};
+
+export type partnerListGetRequest = {
+  userId: string;
+  sort: 'count' | 'name' | 'record';
+  limit: number;
+  start: number;
+};
+
+type partnerData = {
+  userId: string;
+  role: RoleEnum;
+  type: DisabilityEnum;
+  name: string;
+  trainingCnt: number;
+  contestCnt: number;
+};
+
+export type partnerListGetResponse = {
+  sort: string;
+  limit: number;
+  start: number;
+  items: partnerData[];
+};
+
+export type partnerListCountGetRequest = {
+  userId: string;
+};
+
+export type partnerListCountGetResponse = {
+  count: number;
+};
+
+export type eventHistoryGetRequest = {
+  userId: string;
+  sort: 'total' | 'upcoming' | 'end';
+  limit: number;
+  start: number;
+};
+
+type eventData = {
+  eventId: number;
+  eventType: 'Training' | 'Competition';
+  name: string;
+  date: string;
+  recruitStatus: 'UPCOMING' | 'OPEN' | 'CLOSE';
+};
+
+export type eventHistoryGetResponse = {
+  sort: string;
+  limit: number;
+  start: number;
+  items: eventData[];
+};
+
+export type eventHistoryCountGetRequest = {
+  userId: string;
+  //sort 들어가야 되지 않나?
+};
+
+export type eventHistoryCountGetResponse = {
+  count: number;
+};
