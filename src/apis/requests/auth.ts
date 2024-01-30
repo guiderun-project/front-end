81be67cc25ce2c98e19672706cec84d801834211
Pridesd <whwotjr98@naver.com>
2024-01-30 13:35:14 +0900

    feat(auth): set auth api and type

---
+++ src/apis/requests/auth.ts
@@ -0,0 +1,46 @@
+import { axiosInstance, axiosInstanceWithToken } from 'apis/axios';
+import {
+  accessTokenGetResponse,
+  checkDuplicatedPostRequest,
+  checkDuplicatedPostResponse,
+  guideSignupPostRequest,
+  kakaoAuthPostRequest,
+  kakaoAuthPostResponse,
+  signupPostResponse,
+  viSignupPostRequest,
+} from 'apis/types/auth';
+
+class AuthApi {
+  kakaoAuthPost = async ({ code }: kakaoAuthPostRequest) => {
+    return await axiosInstance
+      .post<kakaoAuthPostResponse>(`/oauth/login/kakao?code=${code}`)
+      .then((res) => res.data);
+  };
+
+  viSignupPost = async (signupData: viSignupPostRequest) => {
+    return await axiosInstanceWithToken
+      .post<signupPostResponse>('/signup/vi', signupData)
+      .then((res) => res.data);
+  };
+
+  guideSignupPost = async (signupData: guideSignupPostRequest) => {
+    return await axiosInstanceWithToken
+      .post<signupPostResponse>('/signup/guide', signupData)
+      .then((res) => res.data);
+  };
+
+  accessTokenGet = async () => {
+    return await axiosInstance
+      .get<accessTokenGetResponse>('/user/reissue')
+      .then((res) => res.data.accessToken);
+  };
+
+  checkDuplicatedPost = async (id: checkDuplicatedPostRequest) =>
+    axiosInstanceWithToken
+      .post<checkDuplicatedPostResponse>('/signup/duplicated', id)
+      .then((res) => res.data.unique);
+}
+
+const authApi = new AuthApi();
+
+export default authApi;
---
+++ src/apis/types/auth.ts
@@ -0,0 +1,86 @@
+import { GenderEnum, RoleEnum, RunningGroup } from '@/types/group';
+
+export type kakaoAuthPostRequest = {
+  code: string;
+};
+
+export type kakaoAuthPostResponse = {
+  accessToken: string;
+  //false: new user
+  //true: go to main
+  userStatus: boolean;
+};
+
+export type viSignupPostRequest = {
+  //common section
+  accountId: string;
+  password: string;
+  name: string;
+  gender: GenderEnum;
+  phoneNumber: string;
+  isOpenNumber: boolean;
+  age: number;
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  snsId: string | null;
+  isOpenSns: boolean;
+  //VI section
+  isRunningExp: boolean;
+  guideName: string | null;
+  runningPlace: string | null;
+
+  howToKnow: string[] | null;
+  motive: string | null;
+  hopePrefs: string | null;
+
+  privacy: boolean;
+  portraitRights: boolean;
+};
+
+export type guideSignupPostRequest = {
+  //common section
+  accountId: string;
+  password: string;
+  name: string;
+  gender: GenderEnum;
+  phoneNumber: string;
+  isOpenNumber: boolean;
+  age: number;
+  recordDegree: RunningGroup;
+  detailRecord: string | null;
+  snsId: string | null;
+  isOpenSns: boolean;
+  runningPlace: string | null;
+
+  //guide section
+  isGuideExp: boolean;
+  viName: string | null;
+  viRecord: string | null;
+  viCount: string | null;
+  guidingPace: RunningGroup;
+
+  howToKnow: string[] | null;
+  motive: string | null;
+  hopePrefs: string | null;
+
+  privacy: boolean;
+  portraitRights: boolean;
+};
+
+export type signupPostResponse = {
+  userId: string;
+  accessToken: string;
+  role: RoleEnum;
+};
+
+export type accessTokenGetResponse = {
+  accessToken: string;
+};
+
+export type checkDuplicatedPostRequest = {
+  accountId: string;
+};
+
+export type checkDuplicatedPostResponse = {
+  unique: boolean;
+};
