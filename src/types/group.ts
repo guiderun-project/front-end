ef7c6b0535d93f1d0f0e757cc435fb8604f470ee
Jaeseok <72875894+Pridesd@users.noreply.github.com>
2024-01-31 17:39:16 +0900

    chore(info): init info api (#14)
    * chore(info): init info api
    
    * chore(info): set api type
    
    * feat(info): set info api
    
    * fix(eventHistoryCountGetRequest): add sort params
    
    * refactor(type): set sort type
    
    * refactor(info): change location

--- src/types/group.ts
+++ src/types/group.ts
@@ -24,3 +24,21 @@ export enum DisabilityEnum {
   VI = 'vi',
   GUIDE = 'guide',
 }
+
+export enum EventType {
+  Training = 'TRAINING',
+  Competition = 'COMPETITION',
+}
+
+/**
+ * @enum RecruitStatus 모집 상태
+ * @var Upcoming 모집 예정
+ * @var Open 모집중
+ * @var Close 모집 종료
+ */
+
+export enum RecruitStatus {
+  Upcoming = 'UPCOMING',
+  Open = 'OPEN',
+  Close = 'CLOSE',
+}
