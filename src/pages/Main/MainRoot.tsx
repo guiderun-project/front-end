4fe82884d2521365fa1b805b89fb98c9dfbf24c2
Pridesd <whwotjr98@naver.com>
2024-02-07 19:28:22 +0900

    feat(MainRoot): set Greeting

--- src/pages/Main/MainRoot.tsx
+++ src/pages/Main/MainRoot.tsx
@@ -1,15 +1,78 @@
 import React from 'react';
 
-import { Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 import { NavBar, PageLayout } from '@/components/shared';
 
+//
+//
+//
+
+const TITLE_DATA = {
+  name: '조재석',
+  eventItems: [
+    {
+      name: '동아 마라톤',
+      dDay: 20,
+    },
+    {
+      name: 'KOREA 50K',
+      dDay: 40,
+    },
+  ],
+} as const;
+
+//
+//
+//
+
 const MainRoot: React.FC = () => {
   return (
     <PageLayout>
-      <Typography>메인 루트</Typography>
-      <Outlet />
+      <Stack padding="5rem 0" gap="3.75rem">
+        <Box
+          component="header"
+          display="flex"
+          flexDirection="column"
+          gap="0.75rem"
+        >
+          <Typography component="h1" fontSize="1.5rem" fontWeight={400}>
+            <Typography component="span" fontSize="2rem" fontWeight={600}>
+              {TITLE_DATA.name}
+            </Typography>
+            님 안녕하세요!
+          </Typography>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap="0.25rem"
+            borderLeft="2px solid #AAA"
+            paddingLeft="0.75rem"
+          >
+            {TITLE_DATA.eventItems.map((item) => (
+              <Typography
+                key={`${item.name}-dDay`}
+                component="data"
+                color="#666"
+                fontSize="0.875rem"
+                fontWeight={600}
+              >
+                {item.name}까지 D
+                <Typography
+                  component="span"
+                  fontSize="0.875rem"
+                  fontWeight={700}
+                  color="#3586FF"
+                >
+                  -{item.dDay}
+                </Typography>
+              </Typography>
+            ))}
+          </Box>
+        </Box>
+        <Outlet />
+      </Stack>
       <NavBar />
     </PageLayout>
   );
