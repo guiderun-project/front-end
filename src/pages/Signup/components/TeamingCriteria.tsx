8711b5085e147ca4384818f89f004ce40c090960
Pridesd <whwotjr98@naver.com>
2024-02-17 18:11:02 +0900

    style(TeamingCriteria): set color

--- src/pages/Signup/components/TeamingCriteria.tsx
+++ src/pages/Signup/components/TeamingCriteria.tsx
@@ -3,6 +3,8 @@ import { Card, CardContent, Stack, Typography } from '@mui/material';
 import { useIntl, FormattedMessage } from 'react-intl';
 
 import SignupContentBox from './SignupContentBox';
+import { GroupChip } from '@/components/shared';
+import { RunningGroup } from '@/types/group';
 
 //
 //
@@ -12,6 +14,18 @@ const StyledRunGroupBox = styled.div`
   display: flex;
   align-items: center;
   gap: 1.5rem;
+`;
+
+const StyledContentBox = styled.section`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  gap: 2.5rem;
+  width: 100%;
+  padding: 2.5rem 2rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  background-color: #fff;
 `;
 
 //
@@ -35,142 +49,134 @@ const TeamingCriteria: React.FC = () => {
                 </Typography>
               ))}
           </Stack>
-          <Card>
-            <CardContent
-              aria-lable={intl.formatMessage({ id: 'teamingCriteria.title' })}
-              sx={{
-                padding: '1.5rem 2rem',
-                display: 'flex',
-                justifyContent: 'center',
-                gap: '2.5rem',
-              }}
+          <StyledContentBox
+            aria-lable={intl.formatMessage({ id: 'teamingCriteria.title' })}
+          >
+            <Stack
+              gap="1.5rem"
+              aria-lable={intl.formatMessage({ id: 'teamingCriteria.vi' })}
             >
-              <Stack
-                gap="1.5rem"
-                aria-lable={intl.formatMessage({ id: 'teamingCriteria.vi' })}
-              >
-                <Typography fontWeight={800}>
-                  <FormattedMessage id="common.vi" />
-                </Typography>
-                <Stack>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>A</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.fast"
-                        values={{
-                          minutes: 50,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>B</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.medium"
-                        values={{
-                          minutes1: 51,
-                          minutes2: 56,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>C</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.medium"
-                        values={{
-                          minutes1: 57,
-                          minutes2: 65,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>D</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.another"
-                        values={{
-                          minutes: 66,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>E</Typography>
-                    <Typography>
-                      <FormattedMessage id="teamingCriteria.record.none" />
-                    </Typography>
-                  </StyledRunGroupBox>
-                </Stack>
+              <Typography component="h3" fontWeight={800}>
+                <FormattedMessage id="common.vi" />
+              </Typography>
+              <Stack gap="1rem">
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.A} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.fast"
+                      values={{
+                        minutes: 50,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.B} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.medium"
+                      values={{
+                        minutes1: 51,
+                        minutes2: 56,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.C} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.medium"
+                      values={{
+                        minutes1: 57,
+                        minutes2: 65,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.D} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.another"
+                      values={{
+                        minutes: 66,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.E} type="text" />
+                  <Typography>
+                    <FormattedMessage id="teamingCriteria.record.none" />
+                  </Typography>
+                </StyledRunGroupBox>
               </Stack>
-              <Stack
-                gap="1.5rem"
-                aria-lable={intl.formatMessage({ id: 'teamingCriteria.guide' })}
-              >
-                <Typography fontWeight={800}>
-                  <FormattedMessage id="common.guide" />
-                </Typography>
-                <Stack>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>A</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.fast"
-                        values={{
-                          minutes: 45,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>B</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.medium"
-                        values={{
-                          minutes1: 46,
-                          minutes2: 52,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>C</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.medium"
-                        values={{
-                          minutes1: 53,
-                          minutes2: 59,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>D</Typography>
-                    <Typography>
-                      <FormattedMessage
-                        id="teamingCriteria.record.another"
-                        values={{
-                          minutes: 60,
-                        }}
-                      />
-                    </Typography>
-                  </StyledRunGroupBox>
-                  <StyledRunGroupBox>
-                    <Typography fontWeight={700}>E</Typography>
-                    <Typography>
-                      <FormattedMessage id="teamingCriteria.record.none" />
-                    </Typography>
-                  </StyledRunGroupBox>
-                </Stack>
+            </Stack>
+            <Stack
+              gap="1.5rem"
+              aria-lable={intl.formatMessage({ id: 'teamingCriteria.guide' })}
+            >
+              <Typography component="h3" fontWeight={800}>
+                <FormattedMessage id="common.guide" />
+              </Typography>
+              <Stack gap="1rem">
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.A} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.fast"
+                      values={{
+                        minutes: 45,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.B} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.medium"
+                      values={{
+                        minutes1: 46,
+                        minutes2: 52,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.C} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.medium"
+                      values={{
+                        minutes1: 53,
+                        minutes2: 59,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.D} type="text" />
+                  <Typography>
+                    <FormattedMessage
+                      id="teamingCriteria.record.another"
+                      values={{
+                        minutes: 60,
+                      }}
+                    />
+                  </Typography>
+                </StyledRunGroupBox>
+                <StyledRunGroupBox>
+                  <GroupChip group={RunningGroup.E} type="text" />
+                  <Typography>
+                    <FormattedMessage id="teamingCriteria.record.none" />
+                  </Typography>
+                </StyledRunGroupBox>
               </Stack>
-            </CardContent>
-          </Card>
+            </Stack>
+          </StyledContentBox>
         </Stack>
       }
     />
