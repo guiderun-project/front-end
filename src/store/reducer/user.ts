10e4069cafb0c6226afb570d0186fdeed48fc978
Pridesd <whwotjr98@naver.com>
2024-02-27 09:53:20 +0900

    feat(user): ser reducer

---
+++ src/store/reducer/user.ts
@@ -0,0 +1,33 @@
+import { userInfoGetResponse } from '@/apis/types/info';
+import {
+  DisabilityEnum,
+  GenderEnum,
+  RoleEnum,
+  RunningGroup,
+} from '@/types/group';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+const initialState: userInfoGetResponse = {
+  userId: '',
+  gender: GenderEnum.M,
+  age: 0,
+  name: '',
+  phoneNumber: '',
+  recordDegree: RunningGroup.P,
+  role: RoleEnum.New,
+  snsId: '',
+  type: DisabilityEnum.GUIDE,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUserInfo: (state, action: PayloadAction<userInfoGetResponse>) => {
+      state = action.payload;
+    },
+    resetUserInfo: (state, action: PayloadAction<userInfoGetResponse>) => {
+      state = initialState;
+    },
+  },
+});
