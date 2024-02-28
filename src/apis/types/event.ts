883deb38c77070cfc323d85443cd041ea8ec5b30
Pridesd <whwotjr98@naver.com>
2024-02-28 17:32:32 +0900

    feat(eventApi): add eventPopup

--- src/apis/types/event.ts
+++ src/apis/types/event.ts
@@ -49,18 +49,42 @@ export type eventGetRequest = {
 };
 
 export type eventGetResponse = {
+  eventId: number; //이벤트 아이디
+  type: EventType; //대회인지 훈련인지
+  hasPartner: boolean; //파트너가 있는지 (1차 기준 false)
+  partner: string; //조회하는 사람이 신청했고 파트너가 있으면 이름 노출. (1차 기준 빈 값)
+  title: string; //제목
+  recruitStatus: RecruitStatus; //이벤트 모집 상태
+  submit: boolean; //신청 완료된 이벤트인지 아닌지
+  status: RecruitStatus; //이벤트 진행 상태
+  date: string; //이벤트 시작일
+  startTime: string; //"HH : MM"
+  endTime: string; //"HH : MM"
+  created_at: string; //생성일
+  updated_at: string; //수정일
+  details: string; //상세사항
+  maxNumV: number; //시각장애러너 인원 제한
+  maxNumG: number; //가이드러너 인원 제한
+  place: string; //달리는 장소
+};
+
+export type eventPopupGetRequest = {
   eventId: number;
-  organizer: string;
-  hasPartner: boolean;
-  partner: string;
-  title: string;
-  recruitStatus: RecruitStatus;
-  submit: boolean;
-  status: RecruitStatus;
-  date: string;
-  created_at: string;
-  updated_at?: string;
-  details: string;
+};
+
+export type eventPopupGetResponse = {
+  eventId: number; //이벤트 아이디
+  type: EventType; //대회인지 훈련인지
+  title: string; //제목
+  recruitStatus: RecruitStatus; //이벤트 모집 상태
+  date: string; //이벤트 시작일
+  startTime: string; //"HH : MM"
+  endTime: string; //"HH : MM"
+  viCnt: number; //모집된 vi 수
+  guideCnt: number; //모집된 guide 수
+  place: string; //달리는 장소
+  content: string; //이벤트 내용
+  updatedAt: string; //수정일
 };
 
 export type myEventGetRequest = {
