01dbda38426d3fbe9fb0b1d6cc6daa702458b043
Pridesd <whwotjr98@naver.com>
2024-02-28 18:09:37 +0900

    feat(DetailEventModal): set api

--- src/components/shared/DetailEventModal/DetailEventModal.tsx
+++ src/components/shared/DetailEventModal/DetailEventModal.tsx
@@ -7,32 +7,15 @@ import {
   DialogContent,
   DialogTitle,
   Divider,
+  CircularProgress,
+  Stack,
 } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 
 import { EventChip } from '../EventChip';
 import { EventStatus } from '../EventStatus';
 
-import { EventType, RecruitStatus } from '@/types/group';
-
-export type DetailEventType = {
-  eventId: number; //이벤트 아이디
-  type: EventType;
-  hasPartner: boolean; //파트너가 있는지
-  partner: string; //조회하는 사람이 신청했고 파트너가 있으면 이름 노출.
-  title: string; //제목
-  recruitStatus: RecruitStatus;
-  submit: boolean; //신청 완료된 이벤트인지 아닌지
-  status: RecruitStatus;
-  date: string; //이벤트 시작일
-  startTime: string; //"HH : MM"
-  endTime: string; //"HH : MM"
-  created_at: string; //생성일
-  updated_at: string; //수정일
-  details: string; //상세사항
-  maxNumV: number; //시각장애러너 인원 제한
-  maxNumG: number; //가이드러너 인원 제한
-  place: string;
-};
+import eventApi from '@/apis/requests/event';
 
 interface DetailEventModalProps {
   eventId: number;
@@ -40,40 +23,16 @@ interface DetailEventModalProps {
   onModalClose: () => void;
 }
 
-//
-//
-//
-
-const EVENT_DATA: DetailEventType = {
-  eventId: 1,
-  type: EventType.Training,
-  hasPartner: false,
-  partner: '',
-  title: '동아마라톤 대비반 1회차',
-  place: '잠실보조경기장',
-  recruitStatus: RecruitStatus.Close,
-  maxNumG: 30,
-  maxNumV: 15,
-  submit: true,
-  status: RecruitStatus.Close,
-  date: '2000-01-01',
-  startTime: '09:00',
-  endTime: '11:00',
-  created_at: '2000-01-01',
-  updated_at: '2000-01-01',
-  details:
-    '동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차동아마라톤 대비반 1회차',
-};
-
-//
-//
-//
-
 const DetailEventModal: React.FC<DetailEventModalProps> = ({
-  // eventId,
+  eventId,
   isOpen,
   onModalClose,
 }) => {
+  const { data: eventData, isSuccess } = useQuery({
+    queryKey: ['eventPopupGet', eventApi],
+    queryFn: () => eventApi.eventPopupGet({ eventId }),
+  });
+
   return (
     <Dialog
       open={isOpen}
@@ -101,86 +60,98 @@ const DetailEventModal: React.FC<DetailEventModalProps> = ({
       >
         <ClearOutlined fontSize="large" />
       </IconButton>
-      <DialogTitle display="flex" gap="0.5rem" alignItems="center">
-        <EventChip variant="full" type={EVENT_DATA.type} />
-        <Typography
-          component="h1"
-          id="modal-title"
-          fontSize="1.25rem"
-          fontWeight={700}
-        >
-          {EVENT_DATA.title}
-          <EventStatus status={EVENT_DATA.status} />
-        </Typography>
-      </DialogTitle>
-      <DialogContent
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '1.75rem',
-        }}
-      >
-        <Box display="grid" gridTemplateColumns="1fr 2.5fr">
-          <Typography component="h3" fontWeight={700}>
-            일시
-          </Typography>
-          <Typography display="flex" gap="0.5rem">
-            {EVENT_DATA.date.replace(/-/g, '.')}
-            <Divider orientation="vertical" variant="middle" flexItem />
-            {EVENT_DATA.startTime}~{EVENT_DATA.endTime}
-          </Typography>
-        </Box>
-        <Box display="grid" gridTemplateColumns="1fr 2.5fr">
-          <Typography component="h3" fontWeight={700}>
-            장소
-          </Typography>
-          <Typography>{EVENT_DATA.place}</Typography>
-        </Box>
-        <Box display="grid" gridTemplateColumns="1fr 2.5fr">
-          <Typography component="h3" fontWeight={700}>
-            모집인원
-          </Typography>
-          <Typography display="flex" flexDirection="column" gap="0.5rem">
-            <span>
-              시각장애러너{' '}
-              <span
-                style={{
-                  color: '#DE1313',
-                }}
-              >
-                {EVENT_DATA.maxNumV}
-              </span>
-              명
-            </span>
-            <span>
-              가이드러너{' '}
-              <span
-                style={{
-                  color: '#DE1313',
-                }}
-              >
-                {EVENT_DATA.maxNumG}
-              </span>
-              명
-            </span>
-          </Typography>
-        </Box>
-        <Box
-          paddingTop="0.625rem"
-          display="flex"
-          flexDirection="column"
-          gap="0.5rem"
-        >
-          <Typography component="h3" fontWeight={700}>
-            훈련 상세
-          </Typography>
-          <Box padding="1rem" border="1px solid #D9D9D9" borderRadius="0.5rem">
-            <Typography fontSize="0.8125rem" lineHeight="1.25rem">
-              {EVENT_DATA.details}
+      {eventData && isSuccess ? (
+        <>
+          <DialogTitle display="flex" gap="0.5rem" alignItems="center">
+            <EventChip variant="full" type={eventData.type} />
+            <Typography
+              component="h1"
+              id="modal-title"
+              fontSize="1.25rem"
+              fontWeight={700}
+            >
+              {eventData.title}
+              <EventStatus status={eventData.recruitStatus} />
             </Typography>
-          </Box>
-        </Box>
-      </DialogContent>
+          </DialogTitle>
+          <DialogContent
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '1.75rem',
+            }}
+          >
+            <Box display="grid" gridTemplateColumns="1fr 2.5fr">
+              <Typography component="h3" fontWeight={700}>
+                일시
+              </Typography>
+              <Typography display="flex" gap="0.5rem">
+                {eventData.date.replace(/-/g, '.')}
+                <Divider orientation="vertical" variant="middle" flexItem />
+                {eventData.startTime}~{eventData.endTime}
+              </Typography>
+            </Box>
+            <Box display="grid" gridTemplateColumns="1fr 2.5fr">
+              <Typography component="h3" fontWeight={700}>
+                장소
+              </Typography>
+              <Typography>{eventData.place}</Typography>
+            </Box>
+            <Box display="grid" gridTemplateColumns="1fr 2.5fr">
+              <Typography component="h3" fontWeight={700}>
+                모집인원
+              </Typography>
+              <Typography display="flex" flexDirection="column" gap="0.5rem">
+                <span>
+                  시각장애러너{' '}
+                  <span
+                    style={{
+                      color: '#DE1313',
+                    }}
+                  >
+                    {eventData.viCnt}
+                  </span>
+                  명
+                </span>
+                <span>
+                  가이드러너{' '}
+                  <span
+                    style={{
+                      color: '#DE1313',
+                    }}
+                  >
+                    {eventData.guideCnt}
+                  </span>
+                  명
+                </span>
+              </Typography>
+            </Box>
+            <Box
+              paddingTop="0.625rem"
+              display="flex"
+              flexDirection="column"
+              gap="0.5rem"
+            >
+              <Typography component="h3" fontWeight={700}>
+                훈련 상세
+              </Typography>
+              <Box
+                padding="1rem"
+                border="1px solid #D9D9D9"
+                borderRadius="0.5rem"
+              >
+                <Typography fontSize="0.8125rem" lineHeight="1.25rem">
+                  {eventData.content}
+                </Typography>
+              </Box>
+            </Box>
+          </DialogContent>
+        </>
+      ) : (
+        <Stack alignItems="center" justifyContent="center">
+          <CircularProgress />
+        </Stack>
+      )}
     </Dialog>
   );
 };
