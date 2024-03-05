d1cc38af0c3cae614004db75a42be36be7da662c
Jaeseok <72875894+Pridesd@users.noreply.github.com>
2024-03-05 09:53:11 +0900

    hotfix(SignupForm): set errorSetting (#28)

--- src/pages/Signup/SignupVi.tsx
+++ src/pages/Signup/SignupVi.tsx
@@ -11,6 +11,7 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import axios from 'axios';
 import { Helmet } from 'react-helmet-async';
 import {
   useForm,
@@ -109,6 +110,16 @@ const SignupVi: React.FC = () => {
         isCompleted: 'true',
       });
     } catch (e) {
+      if (
+        axios.isAxiosError<{
+          errorCode: string;
+          message: string;
+        }>(e) &&
+        e.response
+      ) {
+        alert(e.response.data.message);
+        return;
+      }
       alert('에러가 발생했습니다. ');
     }
   };
