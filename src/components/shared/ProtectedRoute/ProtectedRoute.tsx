3821dda82bec721b046d0e40be91de54e3edbf97
Pridesd <whwotjr98@naver.com>
2024-03-19 23:04:26 +0900

    fix(ProtectedRoute): fix get info logic

--- src/components/shared/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/shared/ProtectedRoute/ProtectedRoute.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 import authApi from '@/apis/requests/auth';
+import infoApi from '@/apis/requests/info';
 import { BROWSER_PATH, PREV_PATH_KEY } from '@/constants/path';
 import Loading from '@/pages/Loading';
 import { setAccessToken } from '@/store/reducer/auth';
+import { updateInfo } from '@/store/reducer/user';
+import { RoleEnum } from '@/types/group';
 
 const ProtectedRoute: React.FC = () => {
   const location = useLocation();
   const dispatch = useDispatch();
+  const queryClient = useQueryClient();
 
   const { data, isError, error, isLoading, isSuccess } = useQuery({
     queryKey: ['accessTokenGet'],
@@ -33,6 +37,10 @@ const ProtectedRoute: React.FC = () => {
   React.useEffect(() => {
     if (data?.accessToken) {
       dispatch(setAccessToken(data.accessToken));
+    }
+    if (data?.isExist) {
+      dispatch(updateInfo({ role: RoleEnum.User }));
+      queryClient.invalidateQueries({ queryKey: ['userInfoGet'] });
     }
   }, [data]);
 
