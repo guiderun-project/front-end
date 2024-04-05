d5520b2729d385865e2b6c291e139d1d363a6b37
Pridesd <whwotjr98@naver.com>
2024-04-05 21:15:48 +0900

    chore(msw): initial setting

---
+++ src/mocks/handlers/eventHandlers.ts
@@ -0,0 +1,11 @@
+import { http, HttpHandler, HttpResponse } from 'msw';
+
+//You can add HTTP handler by msw DOCS
+//https://mswjs.io/docs/network-behavior/rest
+
+export const eventHandlers: HttpHandler[] = [
+  //   Example code
+  http.get('/post', () => {
+    return HttpResponse.json();
+  }),
+];
