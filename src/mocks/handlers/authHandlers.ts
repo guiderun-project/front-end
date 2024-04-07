3eafc3b20b2e490a733af57fdc596cfc050f5292
Pridesd <whwotjr98@naver.com>
2024-04-07 16:12:18 +0900

    chore(authHandler): set auth mock server

--- src/mocks/handlers/authHandlers.ts
+++ src/mocks/handlers/authHandlers.ts
@@ -1,11 +1,64 @@
 import { http, HttpHandler, HttpResponse } from 'msw';
 
-//You can add HTTP handler by msw DOCS
-//https://mswjs.io/docs/network-behavior/rest
+import { baseURL } from '@/apis/axios';
+import {
+  accessTokenGetResponse,
+  checkDuplicatedPostRequest,
+  checkDuplicatedPostResponse,
+  guideSignupPostRequest,
+  kakaoAuthPostResponse,
+  signupPostResponse,
+  viSignupPostRequest,
+} from '@/apis/types/auth';
+import { RoleEnum } from '@/types/group';
+
+const SIGN_UP_DATA: signupPostResponse = {
+  accessToken: '123',
+  role: RoleEnum.Admin,
+  userId: '123',
+};
 
 export const authHandlers: HttpHandler[] = [
-  //   Example code
-  http.get('/post', () => {
-    return HttpResponse.json();
+  //kakaoAuthPost
+  http.post<
+    Record<string, never>,
+    Record<string, never>,
+    kakaoAuthPostResponse
+  >(baseURL + '/oauth/login/kakao', () => {
+    return HttpResponse.json({ accessToken: '123', isExist: true });
+  }),
+
+  //viSignupPost
+  http.post<Record<string, never>, viSignupPostRequest, signupPostResponse>(
+    baseURL + 'signup/vi',
+    () => {
+      return HttpResponse.json(SIGN_UP_DATA);
+    },
+  ),
+
+  //guideSignupPost
+  http.post<Record<string, never>, guideSignupPostRequest, signupPostResponse>(
+    baseURL + 'signup/guide',
+    () => {
+      return HttpResponse.json(SIGN_UP_DATA);
+    },
+  ),
+
+  //accessTokenGet
+  http.get<
+    Record<string, never>,
+    Record<string, never>,
+    accessTokenGetResponse
+  >(baseURL + '/oauth/login/reissue', () => {
+    return HttpResponse.json({ accessToken: '123' });
+  }),
+
+  //checkDuplicatedPost
+  http.post<
+    Record<string, never>,
+    checkDuplicatedPostRequest,
+    checkDuplicatedPostResponse
+  >(baseURL + '/signup/duplicated', () => {
+    return HttpResponse.json({ isUnique: true });
   }),
 ];
