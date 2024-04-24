474dba3fcd7657fc563beb2a575ff1a23ba38ed7
Pridesd <whwotjr98@naver.com>
2024-04-24 21:53:15 +0900

    chore(eventHandler): set event

--- src/mocks/handlers/eventHandlers.ts
+++ src/mocks/handlers/eventHandlers.ts
@@ -1,11 +1,29 @@
+import { eventPopupGetResponse } from '@/apis/types/event';
+import { EventType, RecruitStatus } from '@/types/group';
 import { http, HttpHandler, HttpResponse } from 'msw';
 
 //You can add HTTP handler by msw DOCS
 //https://mswjs.io/docs/network-behavior/rest
 
 export const eventHandlers: HttpHandler[] = [
-  //   Example code
-  http.get('/post', () => {
-    return HttpResponse.json();
-  }),
+  //eventPopupGet
+  http.get<{ eventId: string }, Record<string, never>, eventPopupGetResponse>(
+    '/event/pop/:eventId',
+    ({ params }) => {
+      return HttpResponse.json({
+        content: '테스트용 이벤트입니다!',
+        date: '0000-00-00',
+        startTime: '00:00',
+        endTime: '00:00',
+        eventId: Number(params.eventId),
+        viCnt: 22,
+        guideCnt: 10,
+        name: '테스트 훈련',
+        place: '테스트 장소',
+        recruitStatus: RecruitStatus.End,
+        type: EventType.Training,
+        updatedAt: '0000-00-00',
+      });
+    },
+  ),
 ];
