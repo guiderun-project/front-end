e7d767a34f15862ae0573727c1eeb3b6a5b1540f
Pridesd <whwotjr98@naver.com>
2024-05-02 19:35:12 +0900

    feat(FindIdPassword): set Tab layout

--- src/pages/FindIdPassword/index.tsx
+++ src/pages/FindIdPassword/index.tsx
@@ -1,5 +1,67 @@
+import React from 'react';
+
+import { Stack, Tab, Tabs, Typography } from '@mui/material';
+
+export enum FindTabEnum {
+  Id = 'id',
+  Pw = 'pw',
+}
+
 const FindIdPassword: React.FC = () => {
-  return <>FindIdPassword</>;
+  const [selectedTab, setSelectedTab] = React.useState(FindTabEnum.Id);
+
+  /**
+   *
+   */
+  const renderPanel = () => {
+    switch (selectedTab) {
+      case FindTabEnum.Id:
+        return <>아이디 찾기</>;
+      case FindTabEnum.Pw:
+        return <>비밀번호 찾기</>;
+      default:
+        return <>잘못된 페이지 접근</>;
+    }
+  };
+
+  //
+  //
+  //
+
+  return (
+    <Stack
+      boxSizing="border-box"
+      alignItems="center"
+      paddingTop="5rem"
+      gap="3.75rem"
+    >
+      <Typography component="h1" fontSize="2rem" fontWeight="2rem">
+        아이디/비밀번호 찾기
+      </Typography>
+      <Stack gap="2.5rem">
+        <Tabs
+          role="tablist"
+          centered
+          value={selectedTab}
+          onChange={(_, newValue) => setSelectedTab(newValue)}
+        >
+          <Tab
+            id="tab-find-id"
+            role="tab"
+            value={FindTabEnum.Id}
+            label="아이디 찾기"
+          />
+          <Tab
+            id="tab-find-pw"
+            role="tab"
+            value={FindTabEnum.Pw}
+            label="비밀번호 재설정"
+          />
+        </Tabs>
+        {renderPanel()}
+      </Stack>
+    </Stack>
+  );
 };
 
 export default FindIdPassword;
