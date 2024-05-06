5734412ca77ab7296c85df42ad861e5d53818925
Pridesd <whwotjr98@naver.com>
2024-05-06 21:47:55 +0900

    feat(FindPw): set Page

--- src/pages/FindIdPassword/sections/FindPwSection.tsx
+++ src/pages/FindIdPassword/sections/FindPwSection.tsx
@@ -5,9 +5,15 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 import CertificateUserPw from './password/CertificateUserPw';
 import EditUserPw from './password/EditUserPw';
+import { useMutationState } from '@tanstack/react-query';
 
 const FindPwSection: React.FC = () => {
   const [isCertificated, setIsCertificated] = React.useState(false);
+
+  const [isSuccess] = useMutationState({
+    filters: { mutationKey: ['findPwToken'] },
+    select: (mutate) => mutate.state.status === 'success',
+  });
   return (
     <Stack id="tabpanel-find-pw" role="tabpanel" aria-labelledby="tab-find-pw">
       {!isCertificated ? <CertificateUserPw /> : <EditUserPw />}
@@ -21,6 +27,7 @@ const FindPwSection: React.FC = () => {
       >
         {!isCertificated ? (
           <Button
+            disabled={!isSuccess}
             fullWidth
             size="large"
             variant="contained"
@@ -37,7 +44,7 @@ const FindPwSection: React.FC = () => {
             size="large"
             variant="contained"
           >
-            비밀번호 변경 완료!
+            비밀번호 변경
           </Button>
         )}
       </Box>
--- src/pages/FindIdPassword/sections/password/CertificateUserPw.tsx
+++ src/pages/FindIdPassword/sections/password/CertificateUserPw.tsx
@@ -1,34 +1,91 @@
 import { Stack } from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
+import { FormProvider, useForm } from 'react-hook-form';
+
 import InputBox from '../../components/InputBox';
-import styled from '@emotion/styled';
+
+import {
+  CheckCertificationTokenPostRequest,
+  GetCertificationTokenPasswordPostRequest,
+} from '@/apis/types/auth';
+import authApi from '@/apis/requests/auth';
 
 const CertificateUserPw: React.FC = () => {
+  const getTokenMethod = useForm<GetCertificationTokenPasswordPostRequest>();
+  const checkTokenMethod = useForm<CheckCertificationTokenPostRequest>();
+
+  const { mutate: getTokenMutate, isSuccess: isSuccessGetToken } = useMutation({
+    mutationKey: ['getCertificationTokenPasswordPost'],
+    mutationFn: (data: GetCertificationTokenPasswordPostRequest) =>
+      authApi.getCertificationTokenPasswordPost(data),
+  });
+
+  const { mutate: checkTokenMutate } = useMutation({
+    mutationKey: ['findPwToken'],
+    mutationFn: (data: CheckCertificationTokenPostRequest) =>
+      authApi.checkCertificationTokenPost(data),
+    onSuccess: () => {
+      alert('인증되었습니다. ');
+    },
+  });
+
+  /**
+   *
+   */
+  const handlePhoneNumberSubmit = (
+    data: GetCertificationTokenPasswordPostRequest,
+  ) => {
+    getTokenMutate(data);
+  };
+
+  /**
+   *
+   */
+  const handleTokenSubmit = (data: CheckCertificationTokenPostRequest) => {
+    checkTokenMutate(data);
+  };
+
+  //
+  //
+  //
+
   return (
     <Stack gap="1.5rem">
-      <Stack component="form" gap="1.5rem">
-        <InputBox
-          autoFocus
-          isHidenSubmitButton
-          singleline
-          id="id"
-          label="아이디"
-          placeholder="아이디 입력"
-        />
-        <InputBox
-          id="phoneNumber"
-          label="휴대전화번호"
-          placeholder={`휴대 전화번호 입력 ('-'제외)`}
-          submitButtonLabel="인증번호 전송"
-        />
-      </Stack>
-      <form onSubmit={(e) => e.preventDefault}>
-        <InputBox
-          id="certificationNumber"
-          label="인증번호"
-          placeholder="인증번호 입력"
-          submitButtonLabel="확인"
-        />
-      </form>
+      <FormProvider {...getTokenMethod}>
+        <Stack
+          component="form"
+          gap="1.5rem"
+          onSubmit={getTokenMethod.handleSubmit(handlePhoneNumberSubmit)}
+        >
+          <InputBox
+            autoFocus
+            isHidenSubmitButton
+            singleline
+            id="accountId"
+            label="아이디"
+            placeholder="아이디 입력"
+          />
+          <InputBox
+            id="phoneNum"
+            label="휴대전화번호"
+            placeholder={`휴대 전화번호 입력 ('-'제외)`}
+            submitButtonLabel="인증번호 전송"
+          />
+        </Stack>
+      </FormProvider>
+      {isSuccessGetToken && (
+        <FormProvider {...checkTokenMethod}>
+          <form onSubmit={checkTokenMethod.handleSubmit(handleTokenSubmit)}>
+            <InputBox
+              autoFocus
+              id="number"
+              label="인증번호"
+              placeholder="인증번호 입력"
+              submitButtonLabel="확인"
+            />
+          </form>
+        </FormProvider>
+      )}
     </Stack>
   );
 };
