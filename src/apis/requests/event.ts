d788819eae7ce80e0a0ea12de67470d6173d9f83
Pridesd <whwotjr98@naver.com>
2024-05-09 17:23:14 +0900

    feat(MainRoot): set d-day api

--- src/apis/requests/event.ts
+++ src/apis/requests/event.ts
@@ -1,11 +1,25 @@
 import { axiosInstanceWithToken } from '../axios';
-import { EventPopupGetRequest, EventPopupGetResponse } from '../types/event';
+import {
+  EventPopupGetRequest,
+  EventPopupGetResponse,
+  UpcomingEventDdayGetResponse,
+} from '../types/event';
 
 class EventApi {
   eventPopupGet = async ({ eventId }: EventPopupGetRequest) => {
     return await axiosInstanceWithToken
       .get<EventPopupGetResponse>(`/event/pop/${eventId}`)
       .then((res) => res.data);
+  };
+
+  /**
+   *
+   * @returns 메인페이지 상단에 나타나는 사용자가 신청한 이벤트 D-day를 반환
+   */
+  upcomingEventDdayGet = async () => {
+    return await axiosInstanceWithToken
+      .get<UpcomingEventDdayGetResponse>('/event/dday')
+      .then((res) => res.data.eventItems);
   };
 }
 
