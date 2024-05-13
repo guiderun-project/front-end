99477c331239fdd357483762b184531656c8bb05
Pridesd <whwotjr98@naver.com>
2024-05-13 21:53:40 +0900

    feat(Main): set Main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,57 +1,20 @@
 import React from 'react';
 
 import styled from '@emotion/styled';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, CircularProgress, Stack, Typography } from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useQuery } from '@tanstack/react-query';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 import PlanedEventIcon from '@/assets/navBar/all_event_bold_icon.png';
 import MyEventIcon from '@/assets/navBar/my_event_icon.png';
-import { LinkButton, UserBox } from '@/components/shared';
+import { LinkButton, PartnerBox } from '@/components/shared';
 import { BROWSER_PATH } from '@/constants/path';
-import { DisabilityEnum, RoleEnum, RunningGroup } from '@/types/group';
-
-//
-//
-//
-
-const PARTNER_DATA = [
-  {
-    userId: '12345',
-    contestCnt: 1,
-    trainingCnt: 0,
-    name: '조재석',
-    recordDegree: RunningGroup.B,
-    role: RoleEnum.User,
-    type: DisabilityEnum.VI,
-  },
-  {
-    userId: '12345231321',
-    contestCnt: 1,
-    trainingCnt: 0,
-    name: '배두리',
-    recordDegree: RunningGroup.A,
-    role: RoleEnum.User,
-    type: DisabilityEnum.VI,
-  },
-  {
-    userId: '1234554532',
-    contestCnt: 1,
-    trainingCnt: 0,
-    name: '이재건',
-    recordDegree: RunningGroup.C,
-    role: RoleEnum.User,
-    type: DisabilityEnum.GUIDE,
-  },
-  {
-    userId: '12345',
-    contestCnt: 1,
-    trainingCnt: 0,
-    name: '장지은',
-    recordDegree: RunningGroup.A,
-    role: RoleEnum.User,
-    type: DisabilityEnum.GUIDE,
-  },
-];
+import infoApi from '@/apis/requests/info';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/index';
+import { PartnerSort } from '@/types/sort';
 
 //
 //
@@ -59,7 +22,6 @@ const PARTNER_DATA = [
 
 const StyledPartnerBox = styled.div`
   display: flex;
-  align-items: center;
   justify-content: flex-start;
   gap: 0.5rem;
   overflow-x: scroll;
@@ -68,7 +30,47 @@ const StyledPartnerBox = styled.div`
   }
 `;
 
+const StyledPartnerListButton = styled(Link)`
+  min-width: 5.5rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  padding: 1.5rem 1.25rem;
+  text-decoration: none;
+  color: #000;
+`;
+
+//
+//
+//
+
 const Main: React.FC = () => {
+  const userId = useSelector((state: RootState) => state.user.userId);
+
+  const {
+    data: partnerCount,
+    isSuccess: isParnerListCountGetSuccess,
+    isLoading: isParnerListCountGetLoading,
+  } = useQuery({
+    queryKey: ['partnerListCountGet'],
+    queryFn: () => infoApi.partnerListCountGet({ userId }),
+  });
+  const { data: partnerListData, isLoading: isPartnerListGetLoading } =
+    useQuery({
+      queryKey: ['partnerListGet'],
+      queryFn: () =>
+        infoApi.partnerListGet({ userId, sort: PartnerSort.Recent, limit: 2 }),
+      enabled: isParnerListCountGetSuccess,
+    });
+
+  /**
+   *
+   */
   const renderEvent = () => {
     return (
       <Stack gap="1.5rem">
@@ -139,17 +141,43 @@ const Main: React.FC = () => {
           </Link>
         </Box>
 
-        <StyledPartnerBox>
-          {PARTNER_DATA.map((partner) => (
-            <UserBox key={partner.userId} partnerData={partner} />
-          ))}
-        </StyledPartnerBox>
+        {isPartnerListGetLoading || isParnerListCountGetLoading ? (
+          <Stack
+            boxSizing="border-box"
+            alignItems="center"
+            justifyContent="center"
+            padding="1rem"
+          >
+            <CircularProgress size={24} aria-label="내 파트너 불러오는 중" />
+          </Stack>
+        ) : (
+          <StyledPartnerBox>
+            {partnerListData?.map((partner) => (
+              <PartnerBox key={partner.userId} partnerData={partner} />
+            ))}
+            {/* TODO 파트너 리스트 더보기 링크 연결 */}
+            {partnerCount && partnerCount > 2 ? (
+              <StyledPartnerListButton
+                to={BROWSER_PATH.MAIN}
+                aria-label="파트너 목록 더보기"
+              >
+                <ArrowForwardIcon />
+                <Typography fontSize="1.0625rem" fontWeight="700">
+                  더보기
+                </Typography>
+              </StyledPartnerListButton>
+            ) : null}
+          </StyledPartnerBox>
+        )}
       </Stack>
     );
   };
 
   return (
     <Stack gap="2.5rem">
+      <Helmet>
+        <title>Guide run Project</title>
+      </Helmet>
       {renderEvent()}
       {renderPartner()}
     </Stack>
