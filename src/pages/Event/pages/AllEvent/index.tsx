a80a06f53b09ac1b4f93857c4f6cd85737668e19
Pridesd <whwotjr98@naver.com>
2024-05-19 21:47:46 +0900

    feat(AllEvent): set title

--- src/pages/Event/pages/AllEvent/index.tsx
+++ src/pages/Event/pages/AllEvent/index.tsx
@@ -1,7 +1,15 @@
+import { Stack, Typography } from '@mui/material';
+
 import React from 'react';
 
 const AllEvent: React.FC = () => {
-  return <>AllEvent</>;
+  return (
+    <Stack gap="2.5rem">
+      <Typography component="h1" fontSize="1.5rem" fontWeight={700}>
+        전체 이벤트
+      </Typography>
+    </Stack>
+  );
 };
 
 export default AllEvent;
