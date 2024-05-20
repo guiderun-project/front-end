41f661c4379b800216ab0e012a690284eba56067
Pridesd <whwotjr98@naver.com>
2024-05-20 19:53:37 +0900

    feat(EventSearch): set textField

--- src/pages/Event/pages/EventSearch/index.tsx
+++ src/pages/Event/pages/EventSearch/index.tsx
@@ -1,5 +1,29 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment, Stack, TextField } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+
 const EventSearch: React.FC = () => {
-  return <>EventSearch</>;
+  return (
+    <>
+      <Helmet>
+        <title>이벤트 검색 - Guide run Project</title>
+      </Helmet>
+      <Stack gap="2.5rem">
+        <TextField
+          autoFocus
+          autoComplete="off"
+          placeholder="찾고 있는 이벤트를 입력해주세요"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+    </>
+  );
 };
 
 export default EventSearch;
