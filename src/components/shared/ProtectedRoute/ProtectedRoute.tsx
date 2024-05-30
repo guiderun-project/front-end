3a59a95e58ba039dcb4290dfe43c03a9a91bbb54
Pridesd <whwotjr98@naver.com>
2024-05-30 18:15:40 +0900

    enhance(ProtectedRoute): set get userInfo api

--- src/components/shared/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/shared/ProtectedRoute/ProtectedRoute.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useQuery } from '@tanstack/react-query';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
+import infoApi from '@/apis/requests/info';
 import { BROWSER_PATH, PREV_PATH_KEY } from '@/constants/path';
 import { RootState } from '@/store/index';
+import { setUserInfo } from '@/store/reducer/user';
+import Loading from '@/pages/Loading';
 
 const ProtectedRoute: React.FC = () => {
   const location = useLocation();
   const accessToken = useSelector((state: RootState) => state.auth.accessToken);
+  const dispatch = useDispatch();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['userInfoGet'],
+    queryFn: () => infoApi.userInfoGet(),
+    enabled: Boolean(accessToken),
+  });
+
+  React.useEffect(() => {
+    if (data) {
+      dispatch(setUserInfo(data));
+    }
+  }, [data]);
 
   React.useEffect(() => {
     window.localStorage.setItem(PREV_PATH_KEY, location.pathname);
   }, [location]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return accessToken ? (
     <Outlet />
