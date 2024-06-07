71668aab9bce9da7e962995f024dcbe08eccf1e9
Pridesd <whwotjr98@naver.com>
2024-06-07 21:40:21 +0900

    fix(LikeButton): set button

--- src/components/shared/LikeButton/LikeButton.tsx
+++ src/components/shared/LikeButton/LikeButton.tsx
@@ -1,6 +1,6 @@
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { IconButton } from '@mui/material';
+import { CircularProgress, IconButton } from '@mui/material';
 import { useQueryClient } from '@tanstack/react-query';
 
 import { UserProfileGetResponse } from '@/apis/types/info';
@@ -19,30 +19,32 @@ const LikeButton: React.FC<LikeButtonProps> = ({ userid }) => {
 
   const { handleLike } = useLike({ userId: userid });
   if (userData) {
-    <IconButton size="small" onClick={handleLike}>
-      {userData.isLiked ? (
-        <FavoriteIcon
-          fontSize="small"
-          aria-label={`${userData.name}님의 인기도`}
-          aria-selected={userData.isLiked}
-          sx={{
-            color: 'red',
-          }}
-        />
-      ) : (
-        <FavoriteBorderIcon
-          fontSize="small"
-          aria-label={`${userData.name}님의 인기도`}
-          aria-selected={userData.isLiked}
-          sx={{
-            color: '#666',
-          }}
-        />
-      )}
-    </IconButton>;
+    return (
+      <IconButton size="small" onClick={handleLike}>
+        {userData.isLiked ? (
+          <FavoriteIcon
+            fontSize="small"
+            aria-label={`${userData.name}님의 인기도`}
+            aria-selected={userData.isLiked}
+            sx={{
+              color: 'red',
+            }}
+          />
+        ) : (
+          <FavoriteBorderIcon
+            fontSize="small"
+            aria-label={`${userData.name}님의 인기도`}
+            aria-selected={userData.isLiked}
+            sx={{
+              color: '#666',
+            }}
+          />
+        )}
+      </IconButton>
+    );
   }
 
-  return <>사용자 데이터를 가지고 올 수 없습니다.</>;
+  return <CircularProgress />;
 };
 
 export default LikeButton;
