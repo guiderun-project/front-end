229fc5d44d044e1c7af221f3cf8e7e6a7906d38d
Pridesd <whwotjr98@naver.com>
2024-06-12 17:22:12 +0900

    feat(MatchingStandard): set component:

---
+++ src/pages/Event/components/MatchingStandardAccordion.tsx
@@ -0,0 +1,39 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material';
+
+import MatchingStandard from './MatchingStandard';
+
+const MatchingStandardAccordion: React.FC = () => {
+  return (
+    <Accordion
+      elevation={0}
+      sx={{
+        background: '#F8F9FF',
+        border: 'none',
+        '&:before': {
+          display: 'none',
+        },
+      }}
+    >
+      <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+        <Typography fontWeight={700} fontSize="1.0625rem">
+          매칭 기준
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails
+        sx={{
+          backgroundColor: '#F1F3FF',
+        }}
+      >
+        <MatchingStandard />
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default MatchingStandardAccordion;
