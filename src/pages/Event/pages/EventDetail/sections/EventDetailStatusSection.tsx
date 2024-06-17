95242627e2e0c4ad874c9d262b73617e00b1e67e
Pridesd <whwotjr98@naver.com>
2024-06-17 17:19:08 +0900

    refactor(EventDetail): seperate page

--- src/pages/Event/pages/EventDetail/sections/EventDetailStatusSection.tsx
+++ src/pages/Event/pages/EventDetail/sections/EventDetailStatusSection.tsx
@@ -1,4 +1,13 @@
-const EventDetailStatusSection: React.FC = () => {
+import { EventGetResponse } from '@/apis/types/event';
+
+interface EventDetailStatusSectionProps {
+  eventData: EventGetResponse;
+  eventId: number;
+}
+
+const EventDetailStatusSection: React.FC<
+  EventDetailStatusSectionProps
+> = () => {
   return <>EventDetailStatusSection</>;
 };
 
