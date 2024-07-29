9ef9603d6c522e63a3a316e06bf51afc9f732cd0
Pridesd <whwotjr98@naver.com>
2024-07-29 16:58:56 +0900

    feat: set transform date util

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,3 +4,21 @@ export const addOneHour = (time: string) => {
   if (newHours >= 24) newHours = newHours - 24;
   return `${String(newHours).padStart(2, '0')}:${minutes}`;
 };
+
+export const getFullKoreanDate = (date: Date) => {
+  const [year, month, day] = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+  ];
+
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+export const getKoreanHourMonth = (time: string) => {
+  const [hour, minute] = time.split(':').map((t) => Number(t));
+
+  return `${hour.toString().padStart(2, '0')}시 ${minute
+    .toString()
+    .padStart(2, '0')}분`;
+};
