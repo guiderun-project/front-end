bf0a02b6e81aa6ef1176c26052a85cb681429ea4
Pridesd <whwotjr98@naver.com>
2024-08-19 14:35:16 +0900

    feat: set User Type

---
+++ src/types/user.ts
@@ -0,0 +1,37 @@
+import { GenderEnum, RoleEnum, RunningGroup } from './group';
+
+export interface UserType {
+  userId: string;
+  name: string;
+  type: string;
+  role: RoleEnum;
+  gender: GenderEnum;
+  phoneNumber: string;
+  recordDegree: RunningGroup;
+  age: number;
+  snsId: string;
+  isOpenNumber: boolean;
+  isOpenSns: boolean;
+  detailRecord: string;
+
+  runningPlace: string;
+  howToKnow: string[];
+  motive: string;
+  hopePrefs: string;
+
+  privacy: boolean;
+  portraitRights: boolean;
+}
+
+export interface ViType extends UserType {
+  isRunningExp: boolean;
+  guideName?: string;
+}
+
+export interface GuideType extends UserType {
+  isGuideExp: boolean;
+  viName: string;
+  viRecord?: string; //vi 러닝 기록
+  viCount?: string; //상세한 가이드 경험 *선택
+  guidingPace: string; //가이드 가능한 페이스
+}
