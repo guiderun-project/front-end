d63de0e014c6ca372688321d8e4c61850a79118c
Pridesd <whwotjr98@naver.com>
2024-09-05 01:02:28 +0900

    feat: set group maching

--- src/pages/Event/pages/EventDetail/components/AttendGroupBox.tsx
+++ src/pages/Event/pages/EventDetail/components/AttendGroupBox.tsx
@@ -3,6 +3,7 @@ import { Stack } from '@mui/material';
 import AttendGroupAccordion from './AttendGroupAccordion';
 
 import { EventApplyStatusGetResponse } from '@/apis/types/event';
+import { GROUP_LIST_WITHOUT_P } from '@/constants/group';
 import { DisabilityEnum, RunningGroup } from '@/types/group';
 
 interface AttendGroupBoxProps {
@@ -12,37 +13,32 @@ interface AttendGroupBoxProps {
 const AttendGroupBox: React.FC<AttendGroupBoxProps> = ({ member }) => {
   return (
     <Stack gap="1.25rem">
-      {(Object.keys(RunningGroup) as Array<keyof typeof RunningGroup>).map(
-        (group) => {
-          if (group === 'P') return;
-          return (
-            <AttendGroupAccordion
-              key={`AttendGroupBox-${group}`}
-              group={RunningGroup[group]}
-              attendedMemberOfGuide={member.attend.filter(
-                (user) =>
-                  user.applyRecord === RunningGroup[group] &&
-                  user.type === DisabilityEnum.GUIDE,
-              )}
-              attendedMemberOfVi={member.attend.filter(
-                (user) =>
-                  user.applyRecord === RunningGroup[group] &&
-                  user.type === DisabilityEnum.VI,
-              )}
-              notAttendedMemberOfGuide={member.notAttend.filter(
-                (user) =>
-                  user.applyRecord === RunningGroup[group] &&
-                  user.type === DisabilityEnum.GUIDE,
-              )}
-              notAttendedMemberOfVi={member.notAttend.filter(
-                (user) =>
-                  user.applyRecord === RunningGroup[group] &&
-                  user.type === DisabilityEnum.VI,
-              )}
-            />
-          );
-        },
-      )}
+      {GROUP_LIST_WITHOUT_P.map((group) => (
+        <AttendGroupAccordion
+          key={`AttendGroupBox-${group}`}
+          group={RunningGroup[group]}
+          attendedMemberOfGuide={member.attend.filter(
+            (user) =>
+              user.applyRecord === RunningGroup[group] &&
+              user.type === DisabilityEnum.GUIDE,
+          )}
+          attendedMemberOfVi={member.attend.filter(
+            (user) =>
+              user.applyRecord === RunningGroup[group] &&
+              user.type === DisabilityEnum.VI,
+          )}
+          notAttendedMemberOfGuide={member.notAttend.filter(
+            (user) =>
+              user.applyRecord === RunningGroup[group] &&
+              user.type === DisabilityEnum.GUIDE,
+          )}
+          notAttendedMemberOfVi={member.notAttend.filter(
+            (user) =>
+              user.applyRecord === RunningGroup[group] &&
+              user.type === DisabilityEnum.VI,
+          )}
+        />
+      ))}
     </Stack>
   );
 };
