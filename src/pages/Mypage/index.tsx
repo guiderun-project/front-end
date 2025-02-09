a38497fa02fe148108f9cfcf542ddceaf73b9522
Pridesd <whwotjr98@naver.com>
2025-02-09 22:07:11 +0900

    feat: final

--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -28,7 +28,6 @@ import {
 } from '@/components/shared';
 import { BROWSER_PATH } from '@/constants/path';
 import { RootState } from '@/store/index';
-import { resetAccessToken } from '@/store/reducer/auth';
 import { updateInfo } from '@/store/reducer/user';
 import getAuthority from '@/utils/authority';
 
@@ -121,12 +120,11 @@ const Mypage: React.FC = () => {
     },
   });
 
-  const { mutate: logout } = useMutation({
+  const { mutate: handleLogout } = useMutation({
     mutationKey: ['logout'],
     mutationFn: () => authApi.logout(),
     onSuccess: () => {
-      dispatch(resetAccessToken());
-      navigate(BROWSER_PATH.INTRO);
+      navigate(BROWSER_PATH.INTRO, { replace: true });
     },
     onError: () => {
       alert('에러가 발생했습니다. 개발팀한테 뭐라고 좀 해주세요..');
@@ -362,7 +360,7 @@ const Mypage: React.FC = () => {
           fullWidth
           size="large"
           variant="contained"
-          onClick={() => logout()}
+          onClick={() => handleLogout()}
         >
           로그아웃
         </Button>
