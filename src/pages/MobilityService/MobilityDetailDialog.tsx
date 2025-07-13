bff3091648991280b793e948f84a3ab7c4911f58
Pridesd <whwotjr98@naver.com>
2025-07-13 15:54:35 +0900

    feat: add MobilityDetailDialog component for displaying transport service details

---
+++ src/pages/MobilityService/MobilityDetailDialog.tsx
@@ -0,0 +1,74 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  Typography,
+} from '@mui/material';
+
+import { TransportSupport } from '.';
+
+interface MobilityDetailDialogProps {
+  transportService: TransportSupport;
+  onClose: VoidFunction;
+}
+
+const MobilityDetailDialog = ({
+  transportService,
+  onClose,
+}: MobilityDetailDialogProps) => {
+  return (
+    <Dialog open fullWidth onClose={onClose} maxWidth="xl">
+      <DialogTitle>{transportService.name} 이동 지원 연락처</DialogTitle>
+      <DialogContent>
+        <Stack gap="0.5rem">
+          <Stack gap="0.25rem">
+            <Typography component="h2" variant="subtitle1" fontWeight={700}>
+              {transportService.mobilityCenter.name}
+            </Typography>
+            <Stack gap="0.5rem">
+              {transportService.mobilityCenter.contacts.map(
+                (contact, index) => (
+                  <Typography
+                    key={index}
+                    component="a"
+                    href={`tel:${contact.number}`}
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                  >
+                    {`${contact.desc ? `${contact.desc}-` : ''}${
+                      contact.number
+                    }`}
+                  </Typography>
+                ),
+              )}
+            </Stack>
+          </Stack>
+          {transportService.ktx.length > 0 && (
+            <Stack gap="0.25rem">
+              <Typography component="h2" variant="subtitle1" fontWeight={700}>
+                KTX 연락처
+              </Typography>
+              {transportService.ktx.map((ktx, index) => (
+                <Typography
+                  key={index}
+                  component="a"
+                  href={`tel:${ktx.number}`}
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                  {ktx.name} - {ktx.number}
+                </Typography>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>닫기</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default MobilityDetailDialog;
