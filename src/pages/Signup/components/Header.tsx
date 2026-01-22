fc4f1bc205929dd00716641c805a0bcae145b7a4
Jae-seok Jo (조재석) <whwotjr98@naver.com>
2026-01-22 19:31:10 +0900

    feat: Route `BROWSER_PATH.INTRO` to `SignupComplete`, adjust its navigation logic, and add a header layout fix. (#196)

--- src/pages/Signup/components/Header.tsx
+++ src/pages/Signup/components/Header.tsx
@@ -6,18 +6,21 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ children }) => {
   return (
-    <Box
-      component="header"
-      display="flex"
-      justifyContent="center"
-      padding="1.875rem"
-      position="absolute"
-      top="0"
-      right="0"
-      left="0"
-    >
-      <Typography component="h1">{children}</Typography>
-    </Box>
+    <>
+      <Box
+        component="header"
+        display="flex"
+        justifyContent="center"
+        padding="1.875rem"
+        position="absolute"
+        top="0"
+        right="0"
+        left="0"
+      >
+        <Typography component="h1">{children}</Typography>
+      </Box>
+      <Box width="100%" height={84} />
+    </>
   );
 };
 
